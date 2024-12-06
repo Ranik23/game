58cf7f207153c41196fdd212ab1829aef89aada4
anton <antonfedorov190@yandex.ru>
2024-12-06 16:35:41 +0300

    updated

---
+++ cmd/migrator/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"game/internal/config"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+
+
+
+func main() {
+	cfg, err := config.LoadConfig(filepath.Join(os.Getenv("HOME"), "game/config/config.yaml"))
+
+	if err != nil {
+		log.Fatalf("failed to load the config: %v", err)
+	}
+
+	dsn := "host=" + cfg.Postgres.Host + " port=" + cfg.Localhost.Port + " user=" + cfg.Postgres.User + " password=" + cfg.Postgres.Password + " dbname=" + cfg.Postgres.DbName + " sslmode=disable"
+
+
+    m, err := migrate.New(
+        filepath.Join(os.Getenv("HOME"), "game/internal/storage/postgres/migrations"),
+        dsn)
+    if err != nil {
+        log.Fatal(err)
+    }
+    if err := m.Up(); err != nil {
+        log.Fatal(err)
+    }
+}
